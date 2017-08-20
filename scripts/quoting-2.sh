4d6bc812003bc2fed4d6eec4d2633d5a1d74eafb
wa9035148 <wa9035148@gmail.com>
2017-08-20 14:15:10 +0900

    add template.sh

---
+++ scripts/quoting-2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# $'...' construct
+
+# ascii by octol
+echo $'\t \042 \t'
+
+# another way by hexadecimal
+echo $'\t \042 \t'
+
+# ___
+triple_underline=$'\137\137\137'
+echo $triple_underline
